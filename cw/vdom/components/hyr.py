9b463d5d7c17adfca5a8e6ff0448047d88d4233f
Aaron <aaron.dewindt@gmail.com>
2021-05-09 00:28:34 +0200

    vdom.hyr shows types on collapsed items.

--- cw/vdom/components/hyr.py
+++ cw/vdom/components/hyr.py
@@ -30,8 +30,8 @@ def random_string():
 
 
 def hyr(content, show_type=True):
-    _, element = hyr_process(content, show_type)
-    return div(load_css(), element)
+    _, tp, element = hyr_process(content, show_type)
+    return div(load_css(), tp, element, Class="cw_vdom_hyr")
 
 
 def fullname(o):
@@ -51,72 +51,63 @@ def hyr_process(content, show_type):
     if isinstance(content, dict):
         def process_element(key: str):
             option_id = random_string()
-            collapsable, element_content = hyr_process(content[key], show_type)
+            collapsable, tp, element_content = hyr_process(content[key], show_type)
             return div(
                 input_(type="checkbox", id=option_id) if collapsable else "",
-                label(f"{key}: ", For=option_id),
+                label(f"{key}: ", For=option_id), tp,
                 div(element_content),
                 Class="cw_vdom_hyr_item")
 
-        return True, span(type_element, div(
-            *[process_element(key) for key in content],
-            Class="cw_vdom_hyr"
-        ))
+        return True, type_element, div(*[process_element(key) for key in content])
 
     elif isinstance(content, str):
         multiline = "\n" in content
         escaped_content = safe(str(escape(content)).replace("\n", "<br/>"))
         if multiline:
-            return True, span(type_element, div(escaped_content))
+            return True, type_element, div(escaped_content)
         else:
-            return False, span(type_element, span(escaped_content))
+            return False, type_element, span(escaped_content)
 
     # Ordered iterables
     elif isinstance(content, (list, tuple, deque)):
         def process_element(idx):
             option_id = random_string()
-            collapsable, element_content = hyr_process(content[idx], show_type)
+            collapsable, tp, element_content = hyr_process(content[idx], show_type)
             return div(
                 input_(type="checkbox", id=option_id) if collapsable else "",
-                label(f"[{idx}] ", For=option_id),
+                label(f"[{idx}] ", For=option_id), tp,
                 div(element_content),
                 Class="cw_vdom_hyr_item")
 
-        return True, span(type_element, div(
-            *[process_element(idx) for idx in range(len(content))],
-            Class="cw_vdom_hyr"
-        ))
+        return True, type_element, div(*[process_element(idx) for idx in range(len(content))])
 
     # Unordered iterables
     elif isinstance(content, set):
         def process_element(element):
             option_id = random_string()
-            collapsable, element_content = hyr_process(element, show_type)
+            collapsable, tp, element_content = hyr_process(element, show_type)
             return div(
                 input_(type="checkbox", id=option_id) if collapsable else "",
-                label(f"[] ", For=option_id),
+                label(f"[] ", For=option_id), tp,
                 div(element_content),
                 Class="cw_vdom_hyr_item")
 
-        return True, span(type_element, div(
-            *[process_element(element) for element in content],
-            Class="cw_vdom_hyr"
-        ))
+        return True, type_element, div(*[process_element(element) for element in content])
 
     elif isinstance(content, np.ndarray):
         if content.ndim == 1 or (content.ndim == 2 and content.shape[0] == 1):
-            return False, span(type_element, escape(content))
+            return False, type_element, escape(content)
         else:
-            return True, span(type_element, div(safe(str(escape(content)).replace("\n", "<br/>"))))
+            return True, type_element, div(safe(str(escape(content)).replace("\n", "<br/>")))
 
     elif hasattr(content, "_repr_html_") or hasattr(content, "__html__"):
-        return True, span(type_element, div(content))
+        return True, type_element, div(content)
 
     else:
         escaped_content = safe(str(escape(content)).replace("\n", "<br/>"))
         if "<br/>" in escaped_content:
-            return True, span(type_element, div(escaped_content))
+            return True, type_element, div(escaped_content)
         else:
-            return False, span(type_element, span(escaped_content))
+            return False, type_element, span(escaped_content)
 
 
